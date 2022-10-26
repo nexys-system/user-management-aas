76275c084763cfe1aaf4270d7317aea908ff8226
John B <john@nexys.ch>
2022-10-26 21:54:49 +0200

    change exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
-export * as Constants from './constants';
-
-import Service from './service';
+export * as Constants from "./constants";
+export * as Type from "./type";
+export * as Utils from "./utils";
+import Service from "./service";
 
 export default Service;
